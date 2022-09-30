ee5e7c80fc47352f7d4f4eaa74319c102b5e3b7d
Bohndiggin <bohndiggin@gmail.com>
2022-09-30 07:07:43 -0600

    More work done. I can now classify stars based on their temp. I can also calculate Gravitational Force

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const G = 6.67384 * Math.pow(10, -11)
 const GUnits = "m^3kg^-1s^-2"
+const speedOfLight = 299792458
+const oneAU = 149597870700
 
 const solarMass = 1.98847 * Math.pow(10, 30)
 const mercuryMass = 3.3011 * Math.pow(10, 23)
@@ -7,6 +9,16 @@ const earthMass = 5.9722 * Math.pow(10, 24)
 const earthSemiMajorAxis = 149597887 
 const lunarMass = 7.342 * Math.pow(10, 22)
 const lunarSemiMajorAxis = 384399
+
+const starClassArr = [
+    [1000000, 30000, "O"],
+    [30000, 10000, "B"],
+    [10000, 7500, "A"],
+    [7500, 6000, "F"],
+    [6000, 5200, "G"],
+    [5200, 3700, "K"],
+    [3700, 2400, "M"]
+]
 
 function makeSGP (mass1, mass2) { //sgp = standard gravitational parameter
     let sgp = G * (mass1 + mass2)
@@ -23,19 +35,24 @@ function getOrbitalSpeed (mass1, mass2, semiMajorAxis) { //distance must be in k
     return ans * 1000 //km/year
 }
 
-function getOrbitalSpeedKps (mass1, mass2, semiMajorAxis) { // in case you want a normal number not a /year number
+function getOrbitalSpeedKmps (mass1, mass2, semiMajorAxis) { // in case you want a normal number not a /year number
     let ans = getOrbitalSpeed(mass1, mass2, semiMajorAxis)
     ans = ans/(3.154 * Math.pow(10, 7))
     return ans
 }
 
-console.log(getOrbitalSpeedKps(earthMass, solarMass, earthSemiMajorAxis))
+console.log(getOrbitalSpeedKmps(earthMass, solarMass, earthSemiMajorAxis))
 
 function getOrbitalPeriod (mass1, mass2, semiMajorAxis) {
     let orbitalSpeed = getOrbitalSpeed(mass1, mass2, semiMajorAxis)
     let orbitLength = 2 * Math.PI * semiMajorAxis
     let ans = orbitLength / orbitalSpeed
     return (ans) * 366 //days to complete a revolution
+}
+
+function getGravitationalForce (mass1, mass2, distance) { //Not sure if this is needed, but this may come in handy. It calculates the Force between 2 objects.
+    let ans = G * ((mass1 + mass2)/Math.pow(distance, 2))
+    return ans
 }
 
 function update () {
@@ -46,3 +63,14 @@ console.log(getOrbitalSpeed(earthMass, lunarMass, lunarSemiMajorAxis)) // this a
 
 console.log(getOrbitalPeriod(earthMass, solarMass, earthSemiMajorAxis)) // days to revolve around the sun
 console.log(getOrbitalPeriod(earthMass, lunarMass, lunarSemiMajorAxis)) // days to revolve around the earth
+console.log(getGravitationalForce(earthMass, lunarMass, lunarSemiMajorAxis)) //N of force that the Earth-Lunar system has
+
+function classifyStar (starTemp) {
+    for (let i=0; i < starClassArr.length; i++) {
+        if (starTemp >= starClassArr[i][1] && starTemp < starClassArr[i][0]) {
+            console.log(`It's a ${starClassArr[i][2]}-Class Star!`)
+        }
+    }
+}
+
+classifyStar(7500)
