aa6af7a34c572873a0d2ff61fa3ea73cfce9f125
Bohndiggin <bohndiggin@gmail.com>
2022-11-07 08:41:53 -0700

    removed cors to attempt re-deploy

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 require('dotenv').config()
 
 const Sequelize = require('sequelize')
 
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
 const { homeGet, seedFunc, homeJSGet, utilsGet, homeRSCSSGet, homeCSSGet,  calcStarColor, calcEdits, planetAdd, starNumGet, starAdd, starAndPlanets } = require('./controller')
